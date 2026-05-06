87926043c3b11606641ccfd8bbc44c28752830ae
nandhiraja <nandhiraja16@gmail.com>
2026-05-06 23:33:36 +0530

    feat[Services]: add User service  functionality

---
+++ NotificationSystem/Services/UserService.cs
@@ -0,0 +1,109 @@
+using System.Diagnostics;
+using NotificationSystem.Models;
+using NotificationSystem.Repositories;
+namespace NotificationSystem.Services
+{
+    internal class UserService
+    {   
+        UserRepository _userRepository = new UserRepository();
+        static string _userId = "1";
+        public User CreateUserProfile()
+        {   
+            string id = _GenerateUserId();
+
+            Console.WriteLine("\n========================= Create Your Profile =============================\n");
+
+            Console.Write("Please enter your full name : ");
+            string userName = Console.ReadLine()??"";
+            Console.WriteLine("");
+            string userEmail = Console.ReadLine()??"";
+            Console.WriteLine("");
+            string userPhoneNo = Console.ReadLine()??"";
+            
+            Console.WriteLine("\n============================== Thank You ==================================\n");
+
+            User newUser =  new User(id,userName,userEmail,userPhoneNo);
+            return newUser;
+        }
+        public User? EditUser(string email)
+        {
+            User? currentUser = _userRepository.FindUser(email);
+            if(currentUser!=null)
+            {
+            Console.WriteLine("\n========================= Edit Your Profile =============================\n");
+            while(true)
+            {
+            Console.WriteLine("Please enter what you need to edit:\n1.Name\n2.Email\n3.PhoneNo\n4.Save & Exit\n");
+            string editSection =  Console.ReadLine()??"";
+            if(editSection =="1")
+                {
+                    Console.WriteLine($"Current Name: {currentUser.UserName}");
+                    Console.Write("New Name : ");
+                    string newName = Console.ReadLine()??"";
+                    if (newName != "")
+                    {
+                        currentUser.UserName = newName;
+                    }
+                    else
+                        {
+                            Console.WriteLine("Name not be empty..");
+                        }
+                }
+            else if (editSection =="2")
+                {
+                    Console.WriteLine($"Current Email: {currentUser.Email}");
+                    Console.Write("New Email : ");
+                    string newEmail = Console.ReadLine()??"";
+                    if (newEmail != "" && newEmail.Contains("@"))
+                    {
+                        currentUser.Email = newEmail;
+                    }
+                    else
+                        {
+                            Console.WriteLine("Email-Id is invalid to update....");
+                        }
+                }
+            else if (editSection =="3")
+                {
+                    {
+                    Console.WriteLine($"Current Phone Number: {currentUser.Email}");
+                    Console.Write("New PhoneNo : ");
+                    string newPhoneNumber = Console.ReadLine()??"";
+                    if (newPhoneNumber != "" )
+                    {
+                        currentUser.Email = newPhoneNumber;
+                    }
+                    else
+                        {
+                            Console.WriteLine("PhoneNo is invalid to update....");
+                        }
+                }
+                }
+            else if (editSection =="4")
+                {
+                    Console.WriteLine("\n============================== Thank You ==================================\n");
+
+                    return currentUser;
+        
+                }
+            else 
+                {
+                    Console.WriteLine("Enter correct option please...");
+                }
+
+            }
+
+            }
+            return null;
+
+        }
+        string _GenerateUserId()
+        {
+            long previousId = Convert.ToInt64(_userId);
+            string newId =  Convert.ToString(++previousId);
+            _userId = newId;
+            return newId;
+        }
+
+    }
+}
