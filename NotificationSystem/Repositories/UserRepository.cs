d1d9365c4efdbb7ba9c9da24d66cce622a84328d
nandhiraja <nandhiraja16@gmail.com>
2026-05-06 23:35:32 +0530

    feat[Repository]: add Userdata management using CRUD operation

---
+++ NotificationSystem/Repositories/UserRepository.cs
@@ -0,0 +1,64 @@
+using Microsoft.VisualBasic;
+using NotificationSystem.Interfaces;
+using NotificationSystem.Models;
+
+namespace NotificationSystem.Repositories
+{
+    internal class UserRepository: IUserRepository
+    {
+        static List<User> _userDataBase = new List<User>();
+
+        public User AddUser(User user)
+        {   
+            _userDataBase.Add(user);
+            return user;
+        }
+        public User? FindUser(string email)
+        {
+            foreach(var currentUser in _userDataBase)
+            {
+                if(currentUser.Email == email)
+                {
+                  return currentUser;   
+                }
+            }
+            return null;
+        }
+
+        public bool UpdateUser(string email, User newUpdateUser)
+        {
+            int length =_userDataBase.Count;
+
+            for (int i =0;i<length;i++)
+            {
+                if(_userDataBase[i].Email==email)
+                {
+                    _userDataBase[i] = newUpdateUser;
+                    return true;
+                }
+                
+            }
+            return false;
+            
+        }
+
+
+        public User? DeleteUser(string email)
+        {
+            int length =_userDataBase.Count;
+
+            for (int i =0;i<length;i++)
+            {
+                if(_userDataBase[i].Email==email)
+                {   User currentUser = _userDataBase[i];
+                    _userDataBase.RemoveAt(i);
+                    return currentUser;
+                }
+                
+            }
+            return null;
+            
+        }
+
+    }
+}
